0a8737b4b1f1734e5b5a94b617249b6e296cf04d
pfulara <fulara.pawel@gmail.com>
2024-03-05 14:20:58 +0100

    useSearchParams fix

--- src/app/(front)/page.jsx
+++ src/app/(front)/page.jsx
@@ -1,67 +1,10 @@
-'use client';
-import { useEffect, useState, useContext } from 'react';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
-
-import OfferCard from '@/components/OfferCard';
-import NoOffers from '@/components/NoOffers';
-import Filters from './_components/Filters';
-import LoadingIcon from '@/icons/LoadingIcon';
-import { NotificationContext } from '@/providers/NotificationProvider';
+import { Suspense } from 'react';
+import HomePage from './HomePage';
 
 export default function Home() {
-  const { setContext } = useContext(NotificationContext);
-  const [offers, setOffers] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    setLoading(true);
-    const params = {};
-    searchParams.forEach(
-      (value, key) => (params[key] = value)
-    );
-    fetch(`/api/offers`, {
-      cache: 'no-cache',
-      method: 'POST',
-      body: JSON.stringify(params),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.ok) {
-          setContext({
-            status: 'error',
-            message: data.message,
-          });
-        }
-        setOffers(data);
-        setLoading(false);
-      });
-  }, [searchParams, setContext]);
-
   return (
-    <div className='w-full grid grid-cols-6 gap-4'>
-      <div className='block col-span-6 lg:col-span-2'>
-        <Filters />
-      </div>
-      <div className='col-span-6 lg:col-span-4 lg:h-[calc(100vh-theme(height.32))] lg:overflow-y-scroll lg:pr-4'>
-        {isLoading ? (
-          <div className='flex flex-cols w-full h-full items-center justify-center'>
-            <LoadingIcon />
-          </div>
-        ) : !offers.length ? (
-          <NoOffers />
-        ) : (
-          offers?.map((offer) => (
-            <Link
-              key={offer._id}
-              href={`/offer/${offer._id}`}
-            >
-              <OfferCard offer={offer} />
-            </Link>
-          ))
-        )}
-      </div>
-    </div>
+    <Suspense>
+      <HomePage />
+    </Suspense>
   );
 }
